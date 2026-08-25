14873b7d8781b8547f33f32771bbb122231a964f
zrsp1 <ruisheng.zhong@gmail.com>
2026-08-26 05:46:39 +0800

    added player and movement

---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    [SerializeField] private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        Debug.Log("moving");
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+}
